6f1607d78a0a1b27a61a56206951b29ff544182e
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-18 16:57:38 +0200

    First basic  header component has been created

---
+++ src/header.jsx
@@ -0,0 +1,13 @@
+export default function Header()
+{
+	return (
+		<header>
+			<a href="#">PC Store</a>
+			<nav>
+				<span>About</span>
+				<span>Shop</span>
+				<span>Cart</span>
+			</nav>
+		</header>
+	)
+}
