e01deade33b6f7ab957a10e597c52fab4292a012
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-18 17:33:38 +0200

    First example test has been implemented

---
+++ tests/header.test.jsx
@@ -0,0 +1,10 @@
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/react";
+import Header from "../src/header";
+
+describe("Header", () => {
+	it("Renders the basic stuff", () => {
+		const {container} = render(<Header />);
+		expect(container).toMatchSnapshot();
+	})
+})
