1a6e2c0327a53faefe9f3f1c215de33380071a18
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-20 19:48:26 +0200

    Formatted main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,6 @@ const router = createBrowserRouter(routes);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
