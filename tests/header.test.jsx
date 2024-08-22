836d6163fbbfeecd0cbf1a94c9286474d2fc5046
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-22 17:24:52 +0200

    The test now passes again, implementing shop element

--- tests/header.test.jsx
+++ tests/header.test.jsx
@@ -1,10 +1,15 @@
 import { describe, it, expect } from "vitest";
 import { render } from "@testing-library/react";
 import { Header } from "../src/components/homePage";
+import { BrowserRouter } from "react-router-dom";
 
 describe("Header", () => {
 	it("Renders the basic stuff", () => {
-		const { container } = render(<Header />);
+		const { container } = render(
+			<BrowserRouter>
+				<Header />
+			</BrowserRouter>
+		);
 		expect(container).toMatchSnapshot();
 	})
-})
+});
