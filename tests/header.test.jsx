ffec3d20fc3e816cd3dd4cfced45b4730ff221c1
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-26 19:30:27 +0200

    First heading test seems to work, more testing is needed

--- tests/header.test.jsx
+++ tests/header.test.jsx
@@ -1,7 +1,9 @@
 import { describe, it, expect } from "vitest";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { Header } from "../src/components/homePage";
 import { BrowserRouter } from "react-router-dom";
+//import App from "../src/components/App";
+import { HomePage } from "../src/components/homePage";
 
 describe("Header", () => {
 	it("Renders the basic stuff", () => {
@@ -12,4 +14,19 @@ describe("Header", () => {
 		);
 		expect(container).toMatchSnapshot();
 	})
+});
+
+describe("Home page", () => {
+	it("Renders the title page", () => {
+		const description = {
+			exact: false,
+			name : "Looking for hardware? You are at the right place.",
+		}
+		render(
+            <BrowserRouter>
+                <HomePage />
+            </BrowserRouter>
+        );
+		expect(screen.getByRole("heading", description).textContent).toMatch("Looking for hardware? You are at the right place.");
+	});
 });
