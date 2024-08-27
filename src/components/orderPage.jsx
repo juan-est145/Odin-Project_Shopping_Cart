3cf1891d2c6ed29790de998caa95bd41a7acd422
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-27 17:00:56 +0200

    Main logic in cart page works correctly

--- src/components/orderPage.jsx
+++ src/components/orderPage.jsx
@@ -13,7 +13,7 @@ export function OrderPage() {
 	}
 
 	return (
-		<main>
+		<main className="cartDisplay">
 			<section>
 				{Object.entries(order).map(([id, element]) => (
 					<ShopOrder key={id} shopItem={element} setOrder={setOrder}></ShopOrder>
@@ -27,12 +27,21 @@ export function OrderPage() {
 }
 
 function ShopOrder({ shopItem, setOrder }) {
+	function deleteItem() {
+		setOrder((prevOrder) => {
+			const newOrder = {...prevOrder};
+			delete newOrder[shopItem.id];
+			return (newOrder);
+		})
+	}
+
 	return (
 		<div className="orderCard">
 			<img src={shopItem.image} alt={`Photo of a ${shopItem.title}`} />
 			<div>
 				<h3>{shopItem.title}</h3>
 				<p>{`${shopItem.price} x ${shopItem.quantity} = ${shopItem.price * shopItem.quantity} €`}</p>
+				<button onClick={deleteItem}>Remove order</button>
 			</div>
 		</div>
 
@@ -43,9 +52,9 @@ function TotalPayment({order})
 {
 	return (
 		<h1>{`${Object.values(order).reduce((total, amount) => (
-				(total.price * total.quantity) + (amount.price * amount.quantity)
-		))} €`}</h1>
-	)
+			total +	(amount.price * amount.quantity)
+		), 0)} €`}</h1>
+	);
 }
 
 ShopOrder.propTypes = {
