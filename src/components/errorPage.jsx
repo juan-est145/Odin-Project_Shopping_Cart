ac124d94d43d86a0fb22308ed694248da2dd1b2b
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-27 18:57:33 +0200

    Gave styling to error page

--- src/components/errorPage.jsx
+++ src/components/errorPage.jsx
@@ -1,9 +1,16 @@
-
+import { Link } from "react-router-dom";
+import "../styles/errorPage.css";
 
 export function ErrorPage() {
 	return (
-		<div>
-			<h1>This blew up</h1>
-		</div>
+		<main className="errorPage">
+			<h1>Oops, it seems that you got lost</h1>
+			<h3>
+				Do not worry, you can go back to the main page by clicking this button
+			</h3>
+			<Link to="/">
+				<button className="shopBtn">Return</button>
+			</Link>
+		</main>
 	);
 }
