c348a2deec28d65464a0dc697f2f1b4f70fd1556
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-27 18:30:46 +0200

    About page is done

--- src/components/aboutPage.jsx
+++ src/components/aboutPage.jsx
@@ -1,10 +1,18 @@
-import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import "../styles/aboutPage.css";
 
 export function AboutPage() {
 	return (
-		<main>
-			<h1>Work in progress in about page</h1>
-			<Link to="/">Return to main page</Link>
+		<main className="aboutPage">
+			<h1>This is a mock online store website to test the concepts of react router</h1>
+			<p>The info and images from the products come from Fake Store API</p>
+			<p>
+				You can follow me at my github
+				<a href="https://github.com/juan-est145">
+					<FontAwesomeIcon icon={faGithub}></FontAwesomeIcon>
+				</a>
+			</p>
 		</main>
 	);
 }
